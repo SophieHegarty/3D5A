506b76a64d292db83928d831c813978de7fcd03a
SophieHegarty <shegarty874@gmail.com>
2017-11-14 16:24:33 +0000

    Final Working Task
    Task using hardcoded array of 6 example arrays from task sheet.

--- Assignment_1/Task_1.c
+++ Assignment_1/Task_1.c
@@ -1,5 +1,5 @@
 //
-//  Task_10000.c
+//  Task_1.c
 //  Assignment_2
 //
 //  Created by Sophie Hegarty on 13/11/2017.
@@ -12,25 +12,61 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#define arraySize 6
-
-int compareCount = 0;
-int swapCount = 0;
-
+#define compareCount 0
+#define swapCount 0
+#define arraySize 10
 
 
 // A utility function to swap two elements
 void swap(int *x,int *y){
     int temp;
-    if(*x =! *y){
     temp = *x;
     *x = *y;
     *y = temp;
-    swapCount++;
-        printf("Swapped %d %d \n", *x, *y);
-    }
-        printf("No Swapp %d %d \n", *x, *y);
+    swapCount++
+}
 
+
+int partition (int arr[], int low, int high) //uses last number as pivot{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+    
+    for (int j = low; j <= high- 1; j++){
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot){
+            //compareCount++;
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+        //compareCount++;
+    }
+    swap(&arr[i + 1], &arr[high]); //swap pivot and value after largest value smaller than pivot
+    return (i + 1); // return place where pivot was placed
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+ low  --> Starting index,
+ high  --> Ending index */
+void quickSort(int arr[], int low, int high){
+    if (low < high)
+    {
+        int partition_1 = partition(arr, low, high);
+        
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, partition_1 - 1);
+        quickSort(arr, partition_1 + 1, high);
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size){
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("n");
 }
 
 char checkSort(int array[], int SIZE){
@@ -54,64 +90,6 @@ char checkSort(int array[], int SIZE){
     return s;
 }
 
-/* Function to print an array */
-void printArray(int arr[], int size){
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-        printf("\n");
-
-}
-
-//Lomuto partition 
-int partition (int arr[], int low, int high){ //uses last number as pivot{
-    int pivot = arr[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
-    
-    for (int j = low; j <= high- 1; j++){
-        // If current element is smaller than or
-        // equal to pivot
-        if (arr[j] <= pivot){
-            compareCount++;
-            i++;    // increment index of smaller element
-            printf("Inside partition_1_1");
-            swap(&arr[i], &arr[j]);
-            printArray(arr, arraySize);
-        }
-
-        printf("Inside partition_1");
-        printArray(arr, arraySize);
-        compareCount++;
-    }
-
-    swap(&arr[i + 1], &arr[high]); //swap pivot and value after largest value smaller than pivot
-    printf("Inside partition_2");
-    printArray(arr, arraySize);
-    char s = checkSort(arr, arraySize);
-
-    return (i+1); // return place where pivot was placed
-}
-
-
-
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
- low  --> Starting index,
- high  --> Ending index */
-void quickSort(int arr[], int low, int high){
-    if (low < high)
-    {
-        int partition_1 = partition(arr, low, high);
-        printf("Inside quicksort");
-        printArray(arr, arraySize);
-        
-        // Separately sort elements before
-        // partition and after partition, recurssively do each side
-        quickSort(arr, low, partition_1 - 1);
-        quickSort(arr, partition_1 + 1, high);
-    }
-}
-
 
 void display(int array[], char test[], int SIZE){
     quickSort(array, 0, SIZE);
@@ -126,15 +104,16 @@ void display(int array[], char test[], int SIZE){
 }
 
 
-int main( int argc , char *argv[]){
+int main()
+{
 
-    int arr_URV[] = {4, 3, 5, 1, 0, 2};
-    int arr_RV[] = {3, 3, 2, 1, 1, 4};
-    int arr_ASL[] = {0, 1, 2, 3, 4, 5};
-    int arr_DSL[] = {5, 4, 3, 2, 1, 0};
-    int arr_UL[] = {3, 3, 3, 3, 3, 3};
+    int arr_URV[] = {10, 7, 8, 9, 1, 5, 12, 15, 3, 11};
+    int arr_RV[] = {10, 7, 8, 9, 7, 5, 10, 15, 3, 11};
+    int arr_ASL[] = {0, 1, 3, 5, 7, 9, 11, 13, 15, 17};
+    int arr_DSL[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int arr_UL[] = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
     
-    /*quickSort(arr_URV, 0, arraySize);
+    quickSort(arr_URV, 0, arraySize);
     printArray(arr_URV, arraySize);
 
     char sortCheck = checkSort(arr_URV, arraySize);
@@ -142,13 +121,7 @@ int main( int argc , char *argv[]){
     printf("TEST : URV\n" );
     printf("Sorted: %c\n", sortCheck);
     printf("SWAPS: %i\n", swapCount);
-    printf("COMPARES: %i\n", compareCount);*/
-    printArray(arr_URV, arraySize);
-    display(arr_URV, "UniqueRandomValues",  arraySize);
-   // display(arr_RV, "RandomValues", arraySize);
-    //display(arr_ASL, "AscendingSortedList", arraySize);
-    //display(arr_DSL, "DescendingSortedList",  arraySize);
-   // display(arr_UL, "UniformList", arraySize);
+    printf("COMPARES: %i\n", compareCount);
     
     return 0;
 }
