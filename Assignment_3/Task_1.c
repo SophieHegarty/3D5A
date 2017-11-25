960b1c61b2fc102a554a2de87e9f2cc3304a97e1
SophieHegarty <shegarty874@gmail.com>
2017-11-25 22:46:05 +0000

    insert, search and delete functions added
    untested:/

--- Assignment_3/Task_1.c
+++ Assignment_3/Task_1.c
@@ -20,18 +20,41 @@ struct Tree_Node{
     Tree_Node* left, right;
 };
 
-void tree_insert(Tree_Node* root, char data){
-    Tree_Node* new_root = malloc(sizeof(Tree_Node));
-    new_root -> data = data;
-    new_root -> left = NULL;
-    new_root -> right = NULL;
+void tree_insert(Tree_Node** root, char data){
+    Tree_node *temp = NULL;
+    
+    if(!(*root)) {
+        temp = (Tree_node *)malloc(sizeof(node));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *root = temp;
+        return;
+    }
 
-
-
+    if(val < (*root)->data) {
+        insert(&(*root)->left, val);
+     } 
+     else if(val > (*root)->data) {
+        insert(&(*root)->right, val);
+      }
+    
 }
 
-Tree_Node* tree_search(Tree_Node* root, char data){
-
+Tree_Node* tree_search(Tree_Node** root, char data){
+    if(!(*root)) {
+        return NULL;
+    }
+    
+   if(val == (*root)->data) {
+        return *root;
+   } 
+   else if(val < (*root)->data) {
+        search(&((*root)->left), val);
+   } 
+   else if(val > (*root)->data){
+        search(&((*root)->right), val);
+  }
+ 
 }
 
 void tree_print_sorted(Tree_Node* root){
@@ -39,7 +62,11 @@ void tree_print_sorted(Tree_Node* root){
 }
 
 void tree_delete(Tree_Node* root){
-
+    if (root) {
+       deltree(root->left);
+       deltree(root->right);
+       free(root);
+  }
 }
 
 
