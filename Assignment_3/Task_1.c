e28ed01387c9595514cb80dc5e5c9d3dbc65e117
SophieHegarty <shegarty874@gmail.com>
2017-11-29 16:43:53 +0000

    updated

--- Assignment_3/Task_1.c
+++ Assignment_3/Task_1.c
@@ -22,26 +22,26 @@ struct  Tree_Node{
 };
 
 /*void tree_insert(struct Tree_Node** root, char data){
-    struct Tree_Node *temp = NULL;
-    
-    if(!(*root)) {
-        temp = (struct Tree_Node *)malloc(sizeof(struct Tree_Node));
-        temp->left = temp->right = NULL;
-        temp->data = data;
-        *root = temp;
-        return;
-    }
+ struct Tree_Node *temp = NULL;
+ 
+ if(!(*root)) {
+ temp = (struct Tree_Node *)malloc(sizeof(struct Tree_Node));
+ temp->left = temp->right = NULL;
+ temp->data = data;
+ *root = temp;
+ return;
+ }
+ 
+ if(data < (*root)->data) {
+ tree_insert(&(*root)->left, data);
+ }
+ else if(data > (*root)->data) {
+ tree_insert(&(*root)->right, data);
+ }
+ 
+ }*/
 
-    if(data < (*root)->data) {
-        tree_insert(&(*root)->left, data);
-     } 
-     else if(data > (*root)->data) {
-        tree_insert(&(*root)->right, data);
-      }
-    
-}*/
-
-struct Tree_Node* tree_insert(struct Tree_Node *root, int data) {
+struct Tree_Node* tree_insert(struct Tree_Node *root, char data) {
     if (root == NULL) {
         root = (struct Tree_Node *)malloc(sizeof(struct Tree_Node));
         root->data = data;
@@ -52,42 +52,42 @@ struct Tree_Node* tree_insert(struct Tree_Node *root, int data) {
     } else {
         root->right = tree_insert(root->right, data);
     }
-
+    
     return root;
 }
 
 /*struct Tree_Node* tree_search(struct Tree_Node** root, char data){
-    if(!(*root))
-    {
-        return NULL;
-    }
+ if(!(*root))
+ {
+ return NULL;
+ }
+ 
+ if(data < (*root)->data)
+ {
+ tree_search(&((*root)->left), data);
+ }
+ else if(data > (*root)->data)
+ {
+ tree_search(&((*root)->right), data);
+ }
+ else if(data == (*root)->data)
+ {
+ return *root;
+ }
+ }*/
 
-    if(data < (*root)->data)
-    {
-        tree_search(&((*root)->left), data);
-    }
-    else if(data > (*root)->data)
-    {
-        tree_search(&((*root)->right), data);
-    }
-    else if(data == (*root)->data)
-    {
-        return *root;
-    }
-}*/
-
-struct node* search(struct node* root, int key)
+struct Tree_Node* tree_search(struct Tree_Node* root, char data)
 {
     // Base Cases: root is null or key is present at root
-    if (root == NULL || root->key == key)
-       return root;
+    if (root == NULL || root->data == data)
+        return root;
     
     // Key is greater than root's key
-    if (root->key < key)
-       return search(root->right, key);
- 
+    if (root->data < data)
+        return tree_search(root->right, data);
+    
     // Key is smaller than root's key
-    return search(root->left, key);
+    return tree_search(root->left, data);
 }
 
 void tree_print_sorted(struct Tree_Node* root){
@@ -97,15 +97,15 @@ void tree_print_sorted(struct Tree_Node* root){
         tree_print_sorted(root->right);
         printf("%d\n",root->data);
     }
-
+    
 }
 
 void tree_delete(struct Tree_Node* root){
     if (root) {
-       tree_delete(root->left);
-       tree_delete(root->right);
-       free(root);
-  }
+        tree_delete(root->left);
+        tree_delete(root->right);
+        free(root);
+    }
 }
 
 
@@ -113,37 +113,44 @@ void tree_delete(struct Tree_Node* root){
 
 
 int  main ( int argc , char *argv[] ){
-
+    
     struct Tree_Node *root = NULL;
-    char* data = "FLOCCINAUCINIHILIPILIFICATION";
-
+    char data[ARRAY_SIZE] = {"F","L","O","C","C","I","N","A","U","C","I","N","I","H","I","L","I","P","I","L","I","F","I","C","A","T","I","O","N"};
     for(int i = 0; i <ARRAY_SIZE - 1; i++){
+        printf("%c", data[i]);
         root = tree_insert(root, data[i]);
     }
-
+    
     tree_print_sorted(root);
+    
+    char search;
+    
+    
+    printf("Enter letter: ");
+
+    
     
 }
 
 
 
 //https://gist.github.com/aaronjwood/2780f31768691b1d69ed
-/*int vektor[10];    
-int random;
-int uniqueflag;
-int i, j
-
-for(i = 0; i < 10; i++) {
-     do {
-
-        uniqueflag = 1;
-        random = rand() % 100+ 1;
-
-        for (j = 0; j < i && uniqueflag == 1; j++) {
-           if (vektor[j] == random) {
-              uniqueflag = 0;
-           }
-        }
-     } while (uniqueflag != 1);
-     vektor[i] = random;
-}*/
+/*int vektor[10];
+ int random;
+ int uniqueflag;
+ int i, j
+ 
+ for(i = 0; i < 10; i++) {
+ do {
+ 
+ uniqueflag = 1;
+ random = rand() % 100+ 1;
+ 
+ for (j = 0; j < i && uniqueflag == 1; j++) {
+ if (vektor[j] == random) {
+ uniqueflag = 0;
+ }
+ }
+ } while (uniqueflag != 1);
+ vektor[i] = random;
+ }*/
