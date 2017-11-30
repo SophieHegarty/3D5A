1fe0becc5c63af827aff1c82ec133d4685a680d9
SophieHegarty <shegarty874@gmail.com>
2017-11-30 22:56:36 +0000

    updated

--- Assignment_3/task2/src/bstdb.c
+++ Assignment_3/task2/src/bstdb.c
@@ -41,11 +41,12 @@
 
 typedef struct Books Books;
 struct Books {
-	char *title;
-	int word_count;
-	int bookID;
-	Books *left, *right;
-
+    int bookID;
+    char *title;
+    int word_count;
+    
+    Books *left, *right;
+    
 };
 
 Books *root;
@@ -53,33 +54,33 @@ Books *root;
 
 int
 bstdb_init ( void ) {
-	// This function will run once (and only once) when the database first
-	// starts. Use it to allocate any memory you want to use or initialize 
-	// some globals if you need to. Function should return 1 if initialization
-	// was successful and 0 if something went wrong.
-	Books *root = NULL;
-
-	return 1;
+    // This function will run once (and only once) when the database first
+    // starts. Use it to allocate any memory you want to use or initialize
+    // some globals if you need to. Function should return 1 if initialization
+    // was successful and 0 if something went wrong.
+    Books *root = NULL;
+    
+    return 1;
 }
 
 Books* create_Node(Books* title , int word_count, int bookId){
-
-        Books* root = (struct Books*)malloc(sizeof(struct Books));
-		root-> title = title;
-		root -> word_count = word_count;
-		root -> bookID = bookId;
-		root->left = NULL;
-		root->right = NULL;
-
-	/*if (root == NULL) {
-        printf("Error in malloc\n");
-        exit(1);
-    }*/
-	return root;
+    
+    Books* root = (struct Books*)malloc(sizeof(struct Books));
+    root-> title = title;
+    root -> word_count = word_count;
+    root -> bookID = bookId;
+    root->left = NULL;
+    root->right = NULL;
+    
+    /*if (root == NULL) {
+     printf("Error in malloc\n");
+     exit(1);
+     }*/
+    return root;
 }
 
 int create_bookID(char* name){
- int hash = 0;
+    int hash = 0;
     while(*name)
     {
         hash = (hash + *name) % TITLE_SIZE;
@@ -89,7 +90,7 @@ int create_bookID(char* name){
 }
 
 int doublehash(char* name, int bookId){
-	int hash = bookId; //modulous a prime number
+    int hash = bookId; //modulous a prime number
     while(*name)
     {
         hash = ((hash + *name) *31) % TITLE_SIZE;
@@ -99,69 +100,69 @@ int doublehash(char* name, int bookId){
 }
 
 int duplicates(Books* root, int bookID){
-	int check = -1;
-
-	while(root ->bookID != bookID ){
-		if (root == NULL || root->bookID == bookID){
-			check = 1;
-			break;
-		}
-		// Key is greater than root's key
-		else if (root->bookID < bookID){
-			check = 1;
-		} else{
-		// Key is smaller than root's key
-			check = 1;
-		}
-		check = 0;
-	}
-	return check;
+    int check = -1;
+    
+    while(root ->bookID != bookID ){
+        if (root == NULL || root->bookID == bookID){
+            check = 1;
+            break;
+        }
+        // Key is greater than root's key
+        else if (root->bookID < bookID){
+            check = 1;
+        } else{
+            // Key is smaller than root's key
+            check = 1;
+        }
+        check = 0;
+    }
+    return check;
 }
 
 
 int
 bstdb_add ( char *name, int word_count ) {
-	// This function should create a new node in the binary search tree, 
-	// populate it with the name and word_count of the arguments and store
-	// the result in the tree.
-	//
-	// This function should also generate and return an identifier that is
-	// unique to this document. A user can find the stored data by passing
-	// this ID to one of the two search functions below.
-	//
-	// How you generate this ID is up to you, but it must be an integer. Note
-	// that this ID should also form the keys of the nodes in your BST, so
-	// try to generate them in a way that will result in a balanced tree.
-	//
-	// If something goes wrong and the data cannot be stored, this function
-	// should return -1. Otherwise it should return the ID of the new node
-	
-	int bookID = create_bookID(name);
-	int check = -1;
-
-	
-	while(check != 1){
-		bookID = doublehash(name, bookID);
-		check = duplicates(name, bookID);
-	}
-
-
-	if (root == NULL) {
-		root = create_Node(root, word_count, bookID);
-			return bookID;
-		} else if (bookID < root->bookID) {
-
-			root->left = create_Node(root->left, word_count, bookID);
-			
-			return bookID;
-		} else {
-
-			root->right = create_Node(root->right,word_count, bookID);
-
-			return bookID;
-		}
-
-	return -1;
+    // This function should create a new node in the binary search tree,
+    // populate it with the name and word_count of the arguments and store
+    // the result in the tree.
+    //
+    // This function should also generate and return an identifier that is
+    // unique to this document. A user can find the stored data by passing
+    // this ID to one of the two search functions below.
+    //
+    // How you generate this ID is up to you, but it must be an integer. Note
+    // that this ID should also form the keys of the nodes in your BST, so
+    // try to generate them in a way that will result in a balanced tree.
+    //
+    // If something goes wrong and the data cannot be stored, this function
+    // should return -1. Otherwise it should return the ID of the new node
+    
+    int bookID = create_bookID(name);
+    int check = -1;
+    
+    
+    while(check != 1){
+        bookID = doublehash(name, bookID);
+        check = duplicates(name, bookID);
+    }
+    
+    
+    if (root == NULL) {
+        root = create_Node(root, word_count, bookID);
+        return bookID;
+    } else if (bookID < root->bookID) {
+        
+        root->left = create_Node(root->left, word_count, bookID);
+        
+        return bookID;
+    } else {
+        
+        root->right = create_Node(root->right,word_count, bookID);
+        
+        return bookID;
+    }
+    
+    return -1;
 }
 
 struct Books* tree_search(struct Books* root, char bookId)
@@ -180,56 +181,56 @@ struct Books* tree_search(struct Books* root, char bookId)
 
 int
 bstdb_get_word_count ( int doc_id ) {
-	// This is a search function. It should traverse the binary search tree
-	// and return the word_count of the node with the corresponding doc_id.
-	//
-	// If the required node is not found, this function should return -1
-	Books *root = root;
-
-
-	return -1;
+    // This is a search function. It should traverse the binary search tree
+    // and return the word_count of the node with the corresponding doc_id.
+    //
+    // If the required node is not found, this function should return -1
+    Books *root = root;
+    
+    
+    return -1;
 }
 
 char*
 bstdb_get_name ( int doc_id ) {
-	// This is a search function. It should traverse the binary search tree
-	// and return the name of the node with the corresponding doc_id.
-	//
-	// If the required node is not found, this function should return NULL or 0
-	return 0;
+    // This is a search function. It should traverse the binary search tree
+    // and return the name of the node with the corresponding doc_id.
+    //
+    // If the required node is not found, this function should return NULL or 0
+    return 0;
 }
 
 void
 bstdb_stat ( void ) {
-	// Use this function to show off! It will be called once after the 
-	// profiler ends. The profiler checks for execution time and simple errors,
-	// but you should use this function to demonstrate your own innovation.
-	//
-	// Suggestions for things you might want to demonstrate are given below,
-	// but in general what you choose to show here is up to you. This function
-	// counts for marks so make sure it does something interesting or useful.
-	//
-	//  + Check if your tree is balanced and print the result
-	//
-	//  + Does the number of nodes in the tree match the number you expect
-	//    based on the number of insertions you performed?
-	//
-	//  + How many nodes on average did you need to traverse in order to find
-	//    a search result? 
-	//
-	//  + Can you prove that there are no accidental duplicate document IDs
-	//    in the tree?
+    // Use this function to show off! It will be called once after the
+    // profiler ends. The profiler checks for execution time and simple errors,
+    // but you should use this function to demonstrate your own innovation.
+    //
+    // Suggestions for things you might want to demonstrate are given below,
+    // but in general what you choose to show here is up to you. This function
+    // counts for marks so make sure it does something interesting or useful.
+    //
+    //  + Check if your tree is balanced and print the result
+    //
+    //  + Does the number of nodes in the tree match the number you expect
+    //    based on the number of insertions you performed?
+    //
+    //  + How many nodes on average did you need to traverse in order to find
+    //    a search result?
+    //
+    //  + Can you prove that there are no accidental duplicate document IDs
+    //    in the tree?
 }
 
 
 void
 bstdb_quit ( void ) {
-	// This function will run once (and only once) when the program ends. Use
-	// it to free any memory you allocated in the course of operating the
-	// database.
-	  /*if (root) {
-        tree_delete(root->left);
-        tree_delete(root->right);
-        free(root)
-	  }*/
+    // This function will run once (and only once) when the program ends. Use
+    // it to free any memory you allocated in the course of operating the
+    // database.
+    /*if (root) {
+     tree_delete(root->left);
+     tree_delete(root->right);
+     free(root)
+     }*/
 }
