c42343e4b844de9ed89ecc4a83c488f5fb46c13f
SophieHegarty <shegarty874@gmail.com>
2017-12-07 12:40:32 +0000

    task_1_1

---
+++ Assignment_4/Task_1_1.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 5
+
+struct Vertex {
+   char label;
+   bool visited;
+};
+
+//queue variables
+
+int queue[MAX];
+int rear = -1;
+int front = 0;
+int queueItemCount = 0;
+
+//graph variables
+
+//array of vertices
+struct Vertex* lstVertices[MAX];
+
+//adjacency matrix
+int adjMatrix[MAX][MAX];
+
+//vertex count
+int vertexCount = 0;
+
+
+//queue functions
+
+void insert(int data) {
+   queue[++rear] = data;
+   queueItemCount++;
+}
+
+int removeData() {
+   queueItemCount--;
+   return queue[front++]; 
+}
+
+bool isQueueEmpty() {
+   return queueItemCount == 0;
+}
+
+//graph functions
+
+//add vertex to the vertex list
+void addVertex(char label) {
+   struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
+   vertex->label = label;  
+   vertex->visited = false;     
+   lstVertices[vertexCount++] = vertex;
+}
+
+//add edge to edge array
+void addEdge(int start,int end) {
+   adjMatrix[start][end] = 1;
+   adjMatrix[end][start] = 1;
+}
+
+//display the vertex
+void displayVertex(int vertexIndex) {
+   printf("%c ",lstVertices[vertexIndex]->label);
+}       
+
+//get the adjacent unvisited vertex
+int getAdjUnvisitedVertex(int vertexIndex) {
+   int i;
+	
+   for(i = 0; i<vertexCount; i++) {
+      if(adjMatrix[vertexIndex][i] == 1 && lstVertices[i]->visited == false)
+         return i;
+   }
+	
+   return -1;
+}
+
+void breadthFirstSearch() {
+   int i;
+
+   //mark first node as visited
+   lstVertices[0]->visited = true;
+
+   //display the vertex
+   displayVertex(0);   
+
+   //insert vertex index in queue
+   insert(0);
+   int unvisitedVertex;
+
+   while(!isQueueEmpty()) {
+      //get the unvisited vertex of vertex which is at front of the queue
+      int tempVertex = removeData();   
+
+      //no adjacent vertex found
+      while((unvisitedVertex = getAdjUnvisitedVertex(tempVertex)) != -1) {    
+         lstVertices[unvisitedVertex]->visited = true;
+         displayVertex(unvisitedVertex);
+         insert(unvisitedVertex);               
+      }
+		
+   }   
+
+   //queue is empty, search is complete, reset the visited flag        
+   for(i = 0;i<vertexCount;i++) {
+      lstVertices[i]->visited = false;
+   }    
+}
+
+int main() {
+   int i, j;
+
+   for(i = 0; i<MAX; i++) // set adjacency {
+      for(j = 0; j<MAX; j++) // matrix to 0
+         adjMatrix[i][j] = 0;
+   }
+
+   addVertex('S');   // 0
+   addVertex('A');   // 1
+   addVertex('B');   // 2
+   addVertex('C');   // 3
+   addVertex('D');   // 4
+ 
+   addEdge(0, 1);    // S - A
+   addEdge(0, 2);    // S - B
+   addEdge(0, 3);    // S - C
+   addEdge(1, 4);    // A - D
+   addEdge(2, 4);    // B - D
+   addEdge(3, 4);    // C - D
+	
+   
+   printf("\nBreadth First Search: ");
+   
+   breadthFirstSearch();
+
+   return 0;
+}
+
+void struct Graph BFS(int s)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++)
+        visited[i] = false;
+ 
+    // Create a queue for BFS
+    list<int> queue;
+ 
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+ 
+    // 'i' will be used to get all adjacent
+    // vertices of a vertex
+    list<int>::iterator i;
+ 
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        cout << s << " ";
+        queue.pop_front();
+ 
+        // Get all adjacent vertices of the dequeued
+        // vertex s. If a adjacent has not been visited, 
+        // then mark it visited and enqueue it
+        for (i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if (!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+
+BFS (G, s)                   //Where G is the graph and s is the source node
+      let Q be queue.
+      Q.enqueue( s ) //Inserting s in queue until all its neighbour vertices are marked.
+
+      mark s as visited.
+      while ( Q is not empty)
+           //Removing that vertex from queue,whose neighbour will be visited now
+           v  =  Q.dequeue( )
+
+          //processing all the neighbours of v  
+          for all neighbours w of v in Graph G
+               if w is not visited 
+                        Q.enqueue( w )             //Stores w in Q to further visit its neighbour
+                        mark w as visited.
+//https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/tutorial/
